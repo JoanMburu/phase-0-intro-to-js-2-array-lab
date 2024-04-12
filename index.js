a1e1c603dda7d4c2962b3a57ad2442e22d722a64
JoanMburu <joan.wangui@student.moringaschool.com>
2024-04-12 23:31:10 +0300

    updates arrays lab

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution here!
+const cats = ["Milo","Otis","Garfield"];
+function destructivelyAppendCat(){
+    cats.push('Ralph');
+}
+
+function destructivelyPrependCat(){
+    cats.unshift ('Bob');
+}
+
+function destructivelyRemoveLastCat(){
+    return cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift ();
+}
+
+function appendCat(){
+    const copyOfCats = [...cats, "Broom"];
+    return copyOfCats;
+}
+
+function prependCat (){
+    const newCats = ["Arnold",...cats]
+    return newCats;
+}
+
+function removeLastCat (){
+    return cats.slice (0,cats.length -1)
+}
+
+function removeFirstCat (){
+    return cats.slice(1);
+}
